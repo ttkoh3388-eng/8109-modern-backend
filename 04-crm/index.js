957828795bd9d5f930a8a0f95b6cf852778c44e5
ttkoh3388-eng <ttkoh3388@gmail.com>
2026-07-11 18:00:21 +0000

    Part 1

---
+++ 04-crm/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// ejs is a template library
+// it allows us to store html in a file and then send back as response
+const ejs = require('ejs');
+const app = express();
+
+// tell Express that we are using ejs
+app.set("view engine", "ejs");
+
+app.get('/', function(req,res){
+
+    const todayDate = new Date().toLocaleDateString("en-GB");
+    // the first arg to res.render is the name
+    // of the ejs file to send back to the user
+    // and it will be assumed to be in the views folder
+    res.render("home", {
+        "todayDate": todayDate
+    });
+})
+
+app.listen(3000, function(){
+    console.log("Server started");
+})
